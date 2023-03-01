06280197d211d7f3c2608caf98c41af8d46f62e3
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-03-02 00:18:52 +0100

    feat: create `PiCam` class
    The `PiCam` class is used as a high-level API to perform camera-related actions (start / stop streaming, start / stop recording, reload configuration, etc.)

---
+++ picamera/PiCam.py
@@ -0,0 +1,31 @@
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
+
+class PiCam:
+    def __init__(self):
+        self.picam2 = Picamera2()
+        video_config = self.picam2.create_video_configuration(
+            main={"size": (1920, 1080)},
+            lores={"size": (1920, 1080)}
+        )
+        self.picam2.configure(video_config)
+        self.picam2.start()
+
+
+    def start_streaming(self):
+        encoder = H264Encoder()
+        output = FfmpegOutput("-f flv rtmp://192.168.1.211:1935/PiSentry/Spooky_Stream")
+        # output = FfmpegOutput("-f flv rtmp://mediaserver.pisentry.app/PiSentry/Spooky_Stream")
+        try:
+            self.picam2.start_encoder(encoder, output)
+        except Exception as exception:
+            print(f'Exception caught in start_streaming() : {exception}')
+
+    def stop_streaming(self):
+        try:
+            self.picam2.stop_encoder()
+        except Exception as exception:
+            print(f'Exception caught in stop_streaming() : {exception}')
+
+picam = PiCam()
---
+++ picamera/__init__.py
@@ -0,0 +1,1 @@
+from picamera.PiCam import picam
