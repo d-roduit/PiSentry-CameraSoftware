f3b349b5245769004873b74dca597b9d0456bad2
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-06-30 22:17:03 +0200

    refactor: better organize code and avoid too many levels code nesting

--- picamera/PiCam.py
+++ picamera/PiCam.py
@@ -10,6 +10,7 @@ import os
 import cv2
 import time
 
+
 def from_h264_create_file(h264_recording_filepath: str, new_file_extension: str):
     h264_extension = '.h264'
 
@@ -19,7 +20,7 @@ def from_h264_create_file(h264_recording_filepath: str, new_file_extension: str)
     if not new_file_extension.startswith('.'):
         raise ValueError('The new file extension must starts with a point (.)')
 
-    recording_filepath_without_extension = h264_recording_filepath[:-len(h264_extension)]
+    recording_filepath_without_extension = h264_recording_filepath.removesuffix(h264_extension)
     new_filepath = recording_filepath_without_extension + new_file_extension
 
     try:
@@ -34,6 +35,57 @@ def from_h264_create_file(h264_recording_filepath: str, new_file_extension: str)
         )
     except subprocess.TimeoutExpired as timeout_exception:  # program did not finish its task before timeout
         print(f'Process timed out.\n{timeout_exception}')
+
+
+def draw_on_frame(frame, bounding_box, bounding_box_color=(0, 255, 0), object_type=None, confidence=None,
+                  text_color=(0, 125, 0)) -> None:
+    x, y, width, height = bounding_box
+    top_left_corner = (x, y)
+    bottom_right_corner = (x + width, y + height)
+    cv2.rectangle(img=frame, pt1=top_left_corner, pt2=bottom_right_corner, color=bounding_box_color,
+                  thickness=2)
+
+    if object_type is not None:
+        cv2.putText(frame, object_type.upper(), (top_left_corner[0] + 10, top_left_corner[1] + 30),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, text_color, 2)
+
+    if confidence is not None:
+        cv2.putText(frame, str(round(confidence * 100, 2)), (top_left_corner[0] + 200, top_left_corner[1] + 30),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, text_color, 2)
+
+
+def get_object_with_highest_weight_and_area(all_detections, objects_to_detect_with_weights) -> tuple:
+    output = ()
+
+    def bounding_box_area(bounding_box: BoundingBox) -> int:
+        x, y, width, height = bounding_box
+        return height * width
+
+    for detections_in_sub_frame in all_detections:
+        for object_to_check in detections_in_sub_frame:
+            if len(output) == 0:
+                output = object_to_check
+                continue
+
+            object_to_check_type, _, object_to_check_bounding_box = object_to_check
+            output_type, _, output_bounding_box = output
+
+            object_to_check_weight = objects_to_detect_with_weights.get(object_to_check_type)
+            output_weight = objects_to_detect_with_weights.get(output_type)
+
+            if object_to_check_weight > output_weight:
+                output = object_to_check
+                continue
+
+            if object_to_check_weight == output_weight:
+                object_to_check_bounding_box_area = bounding_box_area(object_to_check_bounding_box)
+                output_bounding_box_area = bounding_box_area(output_bounding_box)
+
+                if object_to_check_bounding_box_area > output_bounding_box_area:
+                    output = object_to_check
+
+    return output
+
 
 class PiCam:
     def __init__(self):
@@ -53,6 +105,58 @@ class PiCam:
         self._encoder.output = [self._streamingOutput, self._recordingOutput]
 
         self._h264_recording_filepath = ''
+
+        self._objects_to_detect_with_weights = {
+            'car': 5,
+            'motorcycle': 4,
+            'bicycle': 3,
+            'person': 2,
+            'cat': 1,
+            'dog': 1
+        }
+
+        self._motion_detection_areas: tuple[BoundingBox, ...] = (
+            # (800, 50, 1120, 200),
+            # (800, 350, 1120, 300),
+            #
+            # (250, 0, 200, 200),
+            # (450, 0, 725, 1080),
+            # (0, 500, 450, 1080),
+            # (1175, 300, 350, 1080),
+            # (1525, 350, 250, 1080),
+            # (1775, 400, 250, 300),
+        )
+
+        self._motion_detector = MotionDetector()
+        self._motion_detector.configure_detection(
+            delta_threshold=9,
+            min_area_for_motion=1000,
+            min_frames_for_motion=13
+        )
+
+        directory_path = os.path.dirname(__file__)
+        object_detection_directory = os.path.join(directory_path, 'objectDetection')
+        coco_dataset_file_path = os.path.join(object_detection_directory, 'coco.names')
+        neural_network_weights_file_path = os.path.join(object_detection_directory, 'frozen_inference_graph.pb')
+        neural_network_config_file_path = os.path.join(object_detection_directory,
+                                                       'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
+
+        self._object_detector = ObjectDetector()
+        self._object_detector.configure_dataset(coco_dataset_file_path)
+        self._object_detector.configure_neural_network(
+            neural_network_weights_file_path=neural_network_weights_file_path,
+            neural_network_config_file_path=neural_network_config_file_path,
+            input_size=(320, 320),
+            input_scale=1.0 / 127.5,
+            input_mean=(127.5, 127.5, 127.5),
+            input_swap_rb=True
+        )
+        self._object_detector.configure_detection(
+            confidence_threshold=0.65,
+            non_maximum_suppression_threshold=0.1,
+            objects_to_detect=list(self._objects_to_detect_with_weights.keys()),
+            min_frames_for_detection=5
+        )
 
     def start_streaming(self):
         try:
@@ -80,11 +184,12 @@ class PiCam:
 
             from_h264_create_file(self._h264_recording_filepath, '.mp4')
 
-            if os.path.exists(self._h264_recording_filepath):
-                os.remove(self._h264_recording_filepath)
-                print(f'File "{self._h264_recording_filepath}" removed successfully')
-            else:
+            if not os.path.exists(self._h264_recording_filepath):
                 print(f'File "{self._h264_recording_filepath}" does not exist')
+                return
+
+            os.remove(self._h264_recording_filepath)
+            print(f'File "{self._h264_recording_filepath}" removed successfully')
         except Exception as exception:
             print(f'Exception caught in stop_recording() : {exception}')
 
@@ -92,108 +197,11 @@ class PiCam:
         return self._picam2.capture_array()
 
     def start_detection(self):
-        directory_path = os.path.dirname(__file__)
-        object_detection_directory = os.path.join(directory_path, 'objectDetection')
-        coco_dataset_file_path = os.path.join(object_detection_directory, 'coco.names')
-        neural_network_weights_file_path = os.path.join(object_detection_directory, 'frozen_inference_graph.pb')
-        neural_network_config_file_path = os.path.join(object_detection_directory, 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
-
-        objects_to_detect_with_weights = {
-            'car': 5,
-            'motorcycle': 4,
-            'bicycle': 3,
-            'person': 2,
-            'cat': 1,
-            'dog': 1
-        }
-
-        motion_detection_areas: tuple[BoundingBox, ...] = (
-            # (800, 50, 1120, 200),
-            # (800, 350, 1120, 300),
-            (250, 0, 200, 200),
-            (450, 0, 725, 1080),
-            (0, 500, 450, 1080),
-            (1175, 300, 350, 1080),
-            (1525, 350, 250, 1080),
-            (1775, 400, 250, 300),
-        )
-
-        motionDetector = MotionDetector()
-        objectDetector = ObjectDetector()
-
-        motionDetector.configure_detection(
-            delta_threshold=9,
-            min_area_for_motion=1000,
-            min_frames_for_motion=13
-        )
-
-        objectDetector.configure_dataset(coco_dataset_file_path)
-        objectDetector.configure_neural_network(
-            neural_network_weights_file_path=neural_network_weights_file_path,
-            neural_network_config_file_path=neural_network_config_file_path,
-            input_size=(320, 320),
-            input_scale=1.0 / 127.5,
-            input_mean=(127.5, 127.5, 127.5),
-            input_swap_rb=True
-        )
-        objectDetector.configure_detection(
-            confidence_threshold=0.65,
-            non_maximum_suppression_threshold=0.1,
-            objects_to_detect=list(objects_to_detect_with_weights.keys()),
-            min_frames_for_detection=5
-        )
-
-        frame_number = 0
-
         cv2.startWindowThread()
+        cv2.namedWindow("output", cv2.WINDOW_NORMAL)
 
-        def draw_on_frame(frame, bounding_box, bounding_box_color=(0, 255, 0), object_type=None, confidence=None,
-                          text_color=(0, 125, 0)):
-            x, y, width, height = bounding_box
-            top_left_corner = (x, y)
-            bottom_right_corner = (x + width, y + height)
-            cv2.rectangle(img=frame, pt1=top_left_corner, pt2=bottom_right_corner, color=bounding_box_color,
-                          thickness=2)
-
-            if object_type is not None:
-                cv2.putText(frame, object_type.upper(), (top_left_corner[0] + 10, top_left_corner[1] + 30),
-                            cv2.FONT_HERSHEY_COMPLEX, 1, text_color, 2)
-
-            if confidence is not None:
-                cv2.putText(frame, str(round(confidence * 100, 2)), (top_left_corner[0] + 200, top_left_corner[1] + 30),
-                            cv2.FONT_HERSHEY_COMPLEX, 1, text_color, 2)
-
-        def get_object_with_highest_weight_and_area(all_detections, objects_to_detect_with_weights) -> tuple:
-            output = ()
-
-            def bounding_box_area(bounding_box: BoundingBox) -> int:
-                x, y, width, height = bounding_box
-                return height * width
-
-            for detections_in_sub_frame in all_detections:
-                for object_to_check in detections_in_sub_frame:
-                    if len(output) == 0:
-                        output = object_to_check
-                        continue
-
-                    object_to_check_type, _, object_to_check_bounding_box = object_to_check
-                    output_type, _, output_bounding_box = output
-
-                    object_to_check_weight = objects_to_detect_with_weights.get(object_to_check_type)
-                    output_weight = objects_to_detect_with_weights.get(output_type)
-
-                    if object_to_check_weight > output_weight:
-                        output = object_to_check
-                        continue
-
-                    if object_to_check_weight == output_weight:
-                        object_to_check_bounding_box_area = bounding_box_area(object_to_check_bounding_box)
-                        output_bounding_box_area = bounding_box_area(output_bounding_box)
-
-                        if object_to_check_bounding_box_area > output_bounding_box_area:
-                            output = object_to_check
-
-            return output
+        self._motion_detector.reset_detector()
+        self._object_detector.reset_detector()
 
         RECORDING_PHASE_DURATION_IN_SECONDS = 8
         MAX_RECORDING_PHASE_DURATION_IN_SECONDS = 120
@@ -206,14 +214,8 @@ class PiCam:
         is_in_recording_phase = False  # Recording video of detection
         is_in_checking_phase = False  # Checking if the last object detected is still there
 
-        # grab the array representing the image
+        # Grab the array representing the frame
         while (frame := self.get_frame()) is not None:
-            frame_number += 1
-            # print('\n---------------------------------')
-            # print('Frame no:\t', frame_number)
-            # print('---------------------------------\n')
-            # print('--------- Motion ---------\n')
-
             if is_in_recording_phase:
                 current_time = time.time()
                 seconds_elapsed_since_object_was_detected = current_time - time_when_object_was_detected
@@ -239,78 +241,83 @@ class PiCam:
                     print('-------------- leaving checking phase')
                     previous_object_type_detected = None
 
-            motion_detection = motionDetector.detect(frame=frame, detection_areas=motion_detection_areas)
+            motion_detection = self._motion_detector.detect(frame=frame, detection_areas=self._motion_detection_areas)
 
-            for motion_detection_area in motion_detection_areas:
+            for motion_detection_area in self._motion_detection_areas:
                 draw_on_frame(frame, motion_detection_area, (200, 100, 0))
 
             if len(motion_detection) == 0:
-                pass
-                # print('no motion detection !!')
+                cv2.imshow('output', frame)
+                continue
 
-            if len(motion_detection) > 0:
-                movement_bounding_boxes, _ = motion_detection
+            movement_bounding_boxes, _ = motion_detection
 
-                print('number of movement bounding boxes:', len(movement_bounding_boxes))
+            print('number of movement bounding boxes:', len(movement_bounding_boxes))
 
-                print('\n--------- Object ---------\n')
+            print('\n--------- Object ---------\n')
 
-                object_detection: list[list[tuple]] = objectDetector.detect(frame=frame,
-                                                                            interest_areas=movement_bounding_boxes)
+            object_detection: list[list[tuple]] = self._object_detector.detect(frame=frame,
+                                                                               interest_areas=movement_bounding_boxes)
 
-                for movement_bounding_box in movement_bounding_boxes:
-                    draw_on_frame(frame, movement_bounding_box)
+            for movement_bounding_box in movement_bounding_boxes:
+                draw_on_frame(frame, movement_bounding_box)
 
-                if len(object_detection) > 0:
-                    for object_detections_in_sub_frame in object_detection:
-                        print('\n--------- Object detection in sub frame\n')
+            if len(object_detection) == 0:
+                cv2.imshow('output', frame)
+                continue
 
-                        print('nb objects detected in sub frame:', len(object_detections_in_sub_frame))
+            for object_detections_in_sub_frame in object_detection:
+                print('\n--------- Object detection in sub frame\n')
 
-                        if len(object_detections_in_sub_frame) == 0:
-                            continue
+                print('nb objects detected in sub frame:', len(object_detections_in_sub_frame))
 
-                        for detection_result_of_one_object in object_detections_in_sub_frame:
-                            object_type, object_confidence, object_bounding_box = detection_result_of_one_object
-                            draw_on_frame(frame, object_bounding_box, (0, 125, 0), object_type, object_confidence)
+                if len(object_detections_in_sub_frame) == 0:
+                    continue
 
-                    object_to_notify = get_object_with_highest_weight_and_area(object_detection,
-                                                                               objects_to_detect_with_weights)
+                for detection_result_of_one_object in object_detections_in_sub_frame:
+                    object_type, object_confidence, object_bounding_box = detection_result_of_one_object
+                    draw_on_frame(frame, object_bounding_box, (0, 125, 0), object_type, object_confidence)
 
-                    print('object_with_highest_weight_and_area:', object_to_notify)
+            object_to_notify = get_object_with_highest_weight_and_area(object_detection,
+                                                                       self._objects_to_detect_with_weights)
 
-                    if len(object_to_notify) > 0:
-                        object_to_notify_type, object_to_notify_confidence, object_to_notify_bounding_box = object_to_notify
-                        draw_on_frame(frame, object_to_notify_bounding_box, (0, 100, 200), object_to_notify_type,
-                                      object_to_notify_confidence, (0, 100, 200))
+            print('object_with_highest_weight_and_area:', object_to_notify)
 
-                        print(object_to_notify_type, 'DETECTED')
-                        print('previous_object_type_detected:', previous_object_type_detected)
-                        print('is_in_recording_phase:', is_in_recording_phase)
-                        print('is_in_checking_phase:', is_in_checking_phase)
+            if len(object_to_notify) == 0:
+                cv2.imshow('output', frame)
+                continue
 
-                        must_notify_detection = (
-                                (not is_in_recording_phase and not is_in_checking_phase)
-                                or (is_in_checking_phase and object_to_notify_type != previous_object_type_detected)
-                        )
+            object_to_notify_type, object_to_notify_confidence, object_to_notify_bounding_box = object_to_notify
+            draw_on_frame(frame, object_to_notify_bounding_box, (0, 100, 200), object_to_notify_type,
+                          object_to_notify_confidence, (0, 100, 200))
 
-                        if must_notify_detection:
-                            is_in_checking_phase = False
-                            is_in_recording_phase = True
-                            time_when_recording_phase_started = time.time()
-                            print('-------------- entering recording phase')
-                            self.start_recording()
-                            print('start recording')
-                            # SEND NOTIFICATION OF DETECTION
-                            print('SENDING NOTIFICATION OF DETECTION FOR OJBECT', object_to_notify_type)
+            print(object_to_notify_type, 'DETECTED')
+            print('previous_object_type_detected:', previous_object_type_detected)
+            print('is_in_recording_phase:', is_in_recording_phase)
+            print('is_in_checking_phase:', is_in_checking_phase)
 
-                        elif is_in_checking_phase and object_to_notify_type == previous_object_type_detected:
-                            time_when_checking_phase_started = time.time()
+            must_notify_detection = (
+                    (not is_in_recording_phase and not is_in_checking_phase)
+                    or (is_in_checking_phase and object_to_notify_type != previous_object_type_detected)
+            )
 
-                        time_when_object_was_detected = time.time()
-                        previous_object_type_detected = object_to_notify_type
+            if must_notify_detection:
+                is_in_checking_phase = False
+                is_in_recording_phase = True
+                time_when_recording_phase_started = time.time()
+                print('-------------- entering recording phase')
+                self.start_recording()
+                print('start recording')
+                # SEND NOTIFICATION OF DETECTION
+                print('SENDING NOTIFICATION OF DETECTION FOR OJBECT', object_to_notify_type)
 
-            cv2.namedWindow("output", cv2.WINDOW_NORMAL)
+            elif is_in_checking_phase and object_to_notify_type == previous_object_type_detected:
+                time_when_checking_phase_started = time.time()
+
+            time_when_object_was_detected = time.time()
+            previous_object_type_detected = object_to_notify_type
+
             cv2.imshow('output', frame)
+
 
 picam = PiCam()
