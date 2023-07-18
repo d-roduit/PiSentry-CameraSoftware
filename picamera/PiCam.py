471fe74463e057308f492436fa77d4145c2ed0d6
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-07-18 21:43:19 +0200

    refactor: remove unwanted try...except blocks inside methods

--- picamera/PiCam.py
+++ picamera/PiCam.py
@@ -23,18 +23,9 @@ def from_h264_create_file(h264_recording_filepath: str, new_file_extension: str)
     recording_filepath_without_extension = h264_recording_filepath.removesuffix(h264_extension)
     new_filepath = recording_filepath_without_extension + new_file_extension
 
-    try:
-        subprocess.run(['ffmpeg', '-framerate', '30', '-i', h264_recording_filepath, '-c', 'copy',
-                        f'{new_filepath}'], check=True, timeout=60)
-    except FileNotFoundError as file_not_found_exception:  # one of the program called does not exist
-        print(f'Process failed because the executable could not be found.\n{file_not_found_exception}')
-    except subprocess.CalledProcessError as called_process_exception:  # subprocess execution returned a non-zero code
-        print(
-            f'Process execution did not return a successful return code (0). '
-            f'Returned {called_process_exception.returncode}\n{called_process_exception}'
-        )
-    except subprocess.TimeoutExpired as timeout_exception:  # program did not finish its task before timeout
-        print(f'Process timed out.\n{timeout_exception}')
+    subprocess.run(['ffmpeg', '-framerate', '30', '-i', h264_recording_filepath, '-c', 'copy', f'{new_filepath}'],
+                   check=True, timeout=60)
+
 
 class PiCam:
     def __init__(self):
@@ -58,48 +49,39 @@ class PiCam:
         self._detection_thread = None
 
     def start_streaming(self):
-        try:
-            self._streamingOutput.start()
-        except Exception as exception:
-            print(f'Exception caught in start_streaming() : {exception}')
+        self._streamingOutput.start()
 
     def stop_streaming(self):
-        try:
-            self._streamingOutput.stop()
-        except Exception as exception:
-            print(f'Exception caught in stop_streaming() : {exception}')
+        self._streamingOutput.stop()
 
     def start_recording(self):
-        try:
-            if not os.path.isdir(configManager.config.detection.recordingsFolderPath):
-                raise ValueError('The recordings folder path must point to a directory. Received:', configManager.config.detection.recordingsFolderPath)
+        if not os.path.isdir(configManager.config.detection.recordingsFolderPath):
+            raise ValueError('The recordings folder path must point to a directory. Received:',
+                             configManager.config.detection.recordingsFolderPath)
 
-            if not os.path.isabs(configManager.config.detection.recordingsFolderPath):
-                raise ValueError('The recordings folder path must be an absolute path. Received:', configManager.config.detection.recordingsFolderPath)
+        if not os.path.isabs(configManager.config.detection.recordingsFolderPath):
+            raise ValueError('The recordings folder path must be an absolute path. Received:',
+                             configManager.config.detection.recordingsFolderPath)
 
-            recording_filename = f'recording_{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.h264'
+        recording_filename = f'recording_{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.h264'
 
-            self._h264_recording_filepath = os.path.join(configManager.config.detection.recordingsFolderPath, recording_filename)
+        self._h264_recording_filepath = os.path.join(configManager.config.detection.recordingsFolderPath,
+                                                     recording_filename)
 
-            self._recordingOutput.fileoutput = self._h264_recording_filepath
-            self._recordingOutput.start()
-        except Exception as exception:
-            print(f'Exception caught in start_recording() : {exception}')
+        self._recordingOutput.fileoutput = self._h264_recording_filepath
+        self._recordingOutput.start()
 
     def stop_recording(self):
-        try:
-            self._recordingOutput.stop()
+        self._recordingOutput.stop()
 
-            from_h264_create_file(self._h264_recording_filepath, '.mp4')
+        from_h264_create_file(self._h264_recording_filepath, '.mp4')
 
-            if not os.path.exists(self._h264_recording_filepath):
-                print(f'File "{self._h264_recording_filepath}" does not exist')
-                return
+        if not os.path.exists(self._h264_recording_filepath):
+            print(f'File "{self._h264_recording_filepath}" does not exist')
+            return
 
-            os.remove(self._h264_recording_filepath)
-            print(f'File "{self._h264_recording_filepath}" removed successfully')
-        except Exception as exception:
-            print(f'Exception caught in stop_recording() : {exception}')
+        os.remove(self._h264_recording_filepath)
+        print(f'File "{self._h264_recording_filepath}" removed successfully')
 
     def get_frame(self):
         return self._picam2.capture_array()
@@ -109,6 +91,7 @@ class PiCam:
             return
         self._detection_thread = DetectionThread(self)
         self._detection_thread.start()
+
     def stop_detection(self):
         if self._detection_thread is None or not self.is_detection_running:
             return
@@ -118,5 +101,6 @@ class PiCam:
     @property
     def is_detection_running(self):
         return self._detection_thread.is_alive() if self._detection_thread is not None else False
+
 
 picam = PiCam()
