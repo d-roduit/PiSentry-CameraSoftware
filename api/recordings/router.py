fb92879fb640f313c3abaf448aa1919885531dc2
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-08-12 00:22:31 +0200

    feat: ensure every request to the API receives a response back

--- api/recordings/router.py
+++ api/recordings/router.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, Response, status
 from fastapi.responses import FileResponse
 from picamera import picam
 from ConfigManager import configManager
@@ -12,16 +12,20 @@ router = APIRouter(
 @router.get('/{recording_filename}', response_class=FileResponse)
 def get_recording(recording_filename: str):
     if not os.path.isdir(configManager.config.detection.recordingsFolderPath):
-        raise HTTPException(status_code=500, detail='The recordings folder path must point to a directory')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='The recordings folder path must point to a directory')
 
     recording_filepath = os.path.join(configManager.config.detection.recordingsFolderPath, recording_filename)
 
     if not os.path.isfile(recording_filepath):
-        return HTTPException(status_code=404, detail='Recording not found')
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Recording not found')
 
     return recording_filepath
 
 @router.post('/start')
 def start_recording():
-    picam.start_recording()
+    try:
+        picam.start_recording()
+        return Response(status_code=status.HTTP_200_OK)
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Could not start recording')
 
--- api/streaming/router.py
+++ api/streaming/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Response, status
 from picamera import picam
 
 router = APIRouter(
@@ -8,9 +8,17 @@ router = APIRouter(
 
 @router.post('/start')
 def start_streaming():
-    picam.start_streaming()
+    try:
+        picam.start_streaming()
+        return Response(status_code=status.HTTP_200_OK)
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Could not start streaming')
 
 @router.post('/stop')
 def stop_streaming():
-    picam.stop_streaming()
+    try:
+        picam.stop_streaming()
+        return Response(status_code=status.HTTP_200_OK)
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Could not stop streaming')
 
