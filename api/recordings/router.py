fd8c72d20e9347e3b7f01a798b12f4725398955b
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-08-11 11:56:55 +0200

    feat: remove "GET /recordings" endpoint
    This endpoint was created for test purposes when we did not have the list of existing recordings in the database.
    
    Now that the backend server can retrieve the existing recordings from the DB, as it will work in production, this endpoint is not needed anymore.

--- api/recordings/router.py
+++ api/recordings/router.py
@@ -7,10 +7,6 @@ router = APIRouter(
     tags=['recordings'],
 )
 
-@router.get('/')
-def get_recordings():
-    return { 'recordings': ['mavideo.mp4', 'mavideo2.mp4', 'mavideo3.mp4'] }
-
 @router.get('/{filename}', response_class=FileResponse)
 def get_recording(filename: str):
     return '/home/droduit/Desktop/' + filename
