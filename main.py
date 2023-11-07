a3ee4c78e0ed6aabdf6dd13a0b23bb067329473e
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-11-07 20:49:47 +0100

    feat: start the detection thread when running the main program

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api import streaming, recordings, thumbnails
+from picamera import picam
 
 api = FastAPI()
 
@@ -19,6 +20,8 @@ api.include_router(recordings.router)
 api.include_router(thumbnails.router)
 
 if __name__ == '__main__':
+    picam.start_detection()
+
     async def main():
         config = uvicorn.Config('main:api', host='0.0.0.0', port=9090, reload=True)
         server = uvicorn.Server(config)
