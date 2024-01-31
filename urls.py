90a76f3e0fc255ab90cb1e3d4505068cab1c725f
d-roduit <37080343+d-roduit@users.noreply.github.com>
2024-01-31 13:53:10 +0100

    feat: centralize API endpoints in urls file

--- urls.py
+++ urls.py
@@ -1,1 +1,9 @@
 backend_api_url = 'http://192.168.1.211:7070'
+mediaserver_publish_livestream_url = 'rtmp://192.168.1.211:1935'
+
+cameras_api_endpoint = f'{backend_api_url}/v1/cameras'
+notifications_api_endpoint = f'{backend_api_url}/v1/notifications'
+thumbnails_api_endpoint = f'{backend_api_url}/v1/thumbnails'
+recordings_api_endpoint = f'{backend_api_url}/v1/recordings'
+detection_sessions_api_endpoint = f'{backend_api_url}/v1/detection-sessions'
+detectable_objects_actions_api_endpoint = f'{backend_api_url}/v1/detectable-objects-actions'
