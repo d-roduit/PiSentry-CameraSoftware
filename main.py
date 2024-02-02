56d76bd293447e098c8b20f72c4f11f156e87f8c
d-roduit <37080343+d-roduit@users.noreply.github.com>
2024-02-02 14:25:43 +0100

    feat: create `config` API router

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import streaming, recordings, thumbnails
+from api import streaming, recordings, thumbnails, config
 from picamera import picam
 
 api = FastAPI()
@@ -18,12 +18,13 @@ api.add_middleware(
 api.include_router(streaming.router)
 api.include_router(recordings.router)
 api.include_router(thumbnails.router)
+api.include_router(config.router)
 
 if __name__ == '__main__':
     picam.start_detection()
 
     async def main():
-        config = uvicorn.Config('main:api', host='0.0.0.0', port=9090, reload=True)
-        server = uvicorn.Server(config)
-        await server.serve()
+        uvicorn_config = uvicorn.Config('main:api', host='0.0.0.0', port=9090, reload=True)
+        uvicorn_server = uvicorn.Server(uvicorn_config)
+        await uvicorn_server.serve()
     asyncio.run(main())
